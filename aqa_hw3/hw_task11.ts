e8253597a568304ed2ea0a57c9f7f56bf602b583
shakhlevichA <110105192+shakhlevichA@users.noreply.github.com>
2022-10-23 18:09:53 +0300

    Update hw_task11.ts
    Не могу понять в чём у меня тут ошибка?

--- aqa_hw3/hw_task11.ts
+++ aqa_hw3/hw_task11.ts
@@ -4,6 +4,13 @@
 //     2. Если число делится без остатка на 3 выводим сообшение Buz
 //     3. Если число делится без остатка и на 3 и на 5 выводим сообшение FizBuz
 
-const nameFiz = 'Fiz';//Если число делится без остатка на 5 выводим сообщение Fiz
-const nameBuz = 'Buz';//Если число делится без остатка на 3 выводим сообшение Buz
-const nameFizBuz = 'FizBuz';//Если число делится без остатка и на 3 и на 5 выводим сообшение FizBuz
+const readline = require("readline-sync");
+
+var result:string = readline.question("Type something:");
+if(0 % 5 === 0){
+    console.log('Fiz');
+}else if(0 % 3 ===0){
+    console.log('Buz');
+}else(0 % 3 === 0 && 0 % 5 === 0); {
+    console.log('FizBuz')
+}
