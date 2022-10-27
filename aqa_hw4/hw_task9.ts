7a04e6d95f6a515c2b6856cc44a133b0b21c3766
shakhlevichA <110105192+shakhlevichA@users.noreply.github.com>
2022-10-27 20:38:28 +0300

    Update hw_task9.ts

--- aqa_hw4/hw_task9.ts
+++ aqa_hw4/hw_task9.ts
@@ -8,8 +8,7 @@
 const min:number = Math.ceil(1.543);
 const max:number = Math.floor(9.999);
 const qaz: number = Math.random() * (max - min) + min;
-let newQaz = Number.isInteger(qaz)
-if(newQaz == true){
+if(Number.isInteger(qaz)){
     console.log(qaz)
 }else{
     console.log(Math.floor(qaz))
