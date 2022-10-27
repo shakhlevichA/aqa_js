04ed4bb1e00e8f01a61cf5c98660d44cea0d6387
shakhlevichA <110105192+shakhlevichA@users.noreply.github.com>
2022-10-27 20:33:57 +0300

    Update hw_task3.ts

--- aqa_hw4/hw_task3.ts
+++ aqa_hw4/hw_task3.ts
@@ -10,6 +10,20 @@ if (str.charAt(num)){
    console.log()
 }else console.log(`Вы вышли за границу строки`)
 
-// коряво конечно, но условия выполнил!
-// скинь пожалуйсто пример как оно должно быть 
+let num1 = 53
+let str1 = 'An arbitrary string is given.';
+const symb = str1.charAt(num1);
+if (symb){
+console.log(symb)
+}else console.log('Вы вышли за границу строки')
+//либо так, через тернарный
+let num2 = 53
+let str2 = 'An arbitrary string is given.';
+const symb2 = str2.charAt(num2);
+console.log(symb2 ? symb2 : 'Вы вышли за границу строки')
+
+let num3 = 53
+let str3 = 'An arbitrary string is given.';
+const symb3 = str3.charAt(num2);
+if (!str3.charAt(num3)) console.log('Вы вышли за границу строки')
 
